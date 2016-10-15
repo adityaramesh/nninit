791b52dc942db2e223a8cbaae0765509221c090d
Aditya Ramesh <_@adityaramesh.com>
2016-10-15 01:07:05 -0400

    Make kernel_width an optional parameter for upsampling.

--- test/upsampling_init_test.lua
+++ test/upsampling_init_test.lua
@@ -7,8 +7,8 @@ local img_width = img:size(2)
 image.save('output/original.png', img)
 
 for _, scale in pairs({1, 2, 3, 10, 11}) do
-	local m = nninit.make_spatial_upsampling_conv({kernel_width = 6 * scale - 1,
-		n_maps_in = 3, expansion_factor = 1, scale = scale, kernel = nninit.lanczos(3)})
+	local m = nninit.make_spatial_upsampling_conv({n_maps_in = 3, expansion_factor = 1,
+		scale = scale, kernel = nninit.lanczos(3)})
 
 	local new_width = math.floor(img_width / scale)
 	local x = image.scale(img, new_width, new_width)
